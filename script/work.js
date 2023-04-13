34a11a100986be90123ef002bfbd11b22338b43b
RutabaKashif <rutaba2003@gmail.com>
2023-04-13 01:17:28 -0700

    day 2 work

--- script/work.js
+++ script/work.js
@@ -2,7 +2,7 @@
 
 //Chapter: 01   
 
-alert("Thanks for your input!");
+ /*alert("Thanks for your input!");
 alert ( "Thanks for your input!" );
 
 //Chapter: 02  
@@ -110,4 +110,153 @@ var question = "Your species?";
 var defaultAnswer = "human";
 var spec = prompt(question, defaultAnswer);
 var numberOfCats = prompt("How many cats?");
-var tooManyCats = numberOfCats + 1;
+var tooManyCats = numberOfCats + 1; */
+
+ //Day 2 11 april
+
+
+                             //Chapter: 10
+
+/*var x = prompt("Where does the Pope live?");
+if (x === "Vatican") {
+   alert("Correct!");
+}
+var correctAnswer = "Vatican";
+if (x === correctAnswer) {
+   alert("Correct!");
+}
+var correctAnswer = "Vatican";
+if (x === correctAnswer) {
+   score++;
+   userIQ = "genius";
+   alert("Correct!");
+}
+
+                            / /Chapter:11
+if (fullName === "Mark" + " " + "Myers") {
+if (fullName === firstName + " " + "Myers") {
+if (fullName === firstName + " " + "Myers") {
+ if (fullName === "firstName + " " + lastName) {
+ if (totalCost === 81.50 + 135) 
+  if (totalCost === materialsCost + 135) {
+ if (totalCost === materialsCost + laborCost) {
+ if (x + y === a - b) {
+ if (yourTicketNumber !== 487208) {
+ alert("Better luck next time.");
+ }
+ if (1 > 0) {
+if (0 < 1) {
+ if (1 >= 0) {
+40
+if (1 >= 1) {
+ if (0 <= 1) {
+if (1 <= 1) {
+
+                   //Chapter:12
+var x = prompt("Where does the Pope live?");
+ if (x === "Vatican") {
+  alert("Correct!");
+ var x = prompt("Where does the Pope live?");
+    if (x === "Vatican") {
+alert("Correct!");
+ }
+ if (x !== "Vatican") {
+      alert("Wrong answer");
+}
+ if (x === "Vatican") {
+  alert("Correct!");
+ }
+ else {
+     alert("Wrong answer");
+ }
+ var correctAnswer = "Vatican";
+ if (x === correctAnswer) {
+    alert("Correct!");
+}
+ else {
+    score--;
+    userIQ = "problematic";
+    alert("Incorrect");
+ }
+var correctAnswer = "Vatican";
+if (x === correctAnswer) {
+    43
+    alert("Correct!");
+ }
+ else if (x === "Rome") {
+     alert("Incorrect but close");
+ }
+ else {
+     alert("Incorrect");
+ }
+ var correctAnswer = "Vatican";
+ if (x === correctAnswer) {
+43
+ alert("Correct!");
+ }
+ else if (x === "Rome") {
+alert("Incorrect but close");
+ }
+else {
+alert("Incorrect");
+}
+                                                  
+                   //Chapter 13
+ if (weight > 300 && time < 6) {
+alert("Come to our tryout!");
+ }
+ else {
+ alert("Come to our cookout!");
+  }
+ if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+ alert("Come to our tryout!");
+ }
+ else {
+  alert("Come to our cookout!");
+  }
+ if (SAT > avg || GPA > 2.5 || sport === "football") {
+ alert("Welcome to Bubba State!");
+ }
+ else {
+alert("Have you looked into appliance repair?"); }
+if (age > 65 || age < 21 && res === "U.S.") {
+if ((age > 65 || age < 21) && res === "U.S.") {
+  if (age > 65 || (age < 21 && res === "U.S.")) {
+                                                                      
+                          //Chapter 14
+ if ((x === y || a === b) && c === d) {
+  g = h;
+ }
+  else {
+  e = f;
+ if (c === d) {
+ if (x === y) {
+   g = h;
+ }
+ else if (a === b) {
+ g = h;
+ }
+ else {
+ e = f;
+ }
+}
+else {
+ e = f;
+ }
+                                                                               
+                      //Chapter 15
+ var city0 = "Atlanta";
+ var city1 = "Baltimore";
+ var city2 = "Chicago";
+ var city3 = "Denver";
+ var city4 = "Los Angeles";
+var city5 = "Seattle";
+  alert("Welcome to " + city3);
+  var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+   alert("Welcome to " + cities[3]);
+  var mixedArray = [1, "Bob", "Now is", true];*/
+
+
+
+
+                                                  
